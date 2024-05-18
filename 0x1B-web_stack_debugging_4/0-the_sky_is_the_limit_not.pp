52c8dc070f111524d538f514521d6809bfe2044a
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-05-18 23:40:22 +0100

    h

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,19 +1,6 @@
-# Puppet manifest to optimize Nginx server for high load
+# fix nginx to accept and serve more requests
 
-exec { 'increase_nginx_limits':
-  command => 'sed -i "/^http {/a \\    client_max_body_size 10M;\n    keepalive_timeout 65;" /etc/nginx/nginx.conf',
-  unless  => 'grep -q "client_max_body_size 10M" /etc/nginx/nginx.conf',
-  notify  => Exec['reload_nginx'],
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-exec { 'increase_worker_processes':
-  command => 'sed -i "/^http {/a \\    worker_processes auto;\n    events {\n        worker_connections 1024;\n    }" /etc/nginx/nginx.conf',
-  unless  => 'grep -q "worker_processes auto" /etc/nginx/nginx.conf',
-  notify  => Exec['reload_nginx'],
-}
-
-exec { 'reload_nginx':
-  command     => 'service nginx reload',
-  refreshonly => true,
-}
-
